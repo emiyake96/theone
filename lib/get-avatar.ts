4e9c198166d43730f21b533e15e166d18f73abe9
ericmiyake <emiyake96@github.com>
2026-06-30 21:52:16 -0700

    fix: getAvatar accepts null

--- lib/get-avatar.ts
+++ lib/get-avatar.ts
@@ -1,3 +1,3 @@
-export function getAvatar(userPicture: string | null, userEmail: string) {
-    return userPicture ?? `https://avatar.vercel.sh/${userEmail}`
+export function getAvatar(userPicture: string | null, userEmail: string | null) {
+    return userPicture ?? `https://avatar.vercel.sh/${userEmail ?? 'user'}`
 }
